97dc41cd2220b74b412e86d7d3e6a791e067873e
Faraz Khalid <farazkhalid05@yahoo.com>
2020-05-15 15:55:50 +0500

    basic strcture is made (random is yet to be implemented)

--- Assets/Scripts/QnA_json.cs
+++ Assets/Scripts/QnA_json.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 using System.IO;
 using UnityEngine.UI;
 using System;
+using UnityEngine.EventSystems;
 
 
 //public class QnA_json : MonoBehaviour
@@ -163,73 +164,119 @@ public class QnA_json : MonoBehaviour
     string path;
     int qCounter = 0;
     public Button[] b;
+
     public GameObject Wrong_Panel;
+    public GameObject Correct_Panel;
+    public GameObject Welcome_Panel;
+
+
+    public System.Random a = new System.Random(); // replace from new Random(DateTime.Now.Ticks.GetHashCode());
+                                                 // Since similar code is done in default constructor internally
+    public List<int> randomList = new List<int>();
+    int MyNumber = 0;
+
 
     // Start is called before the first frame update
     void Start()
     {
-        
-        //QnA q1 = new QnA(); 
-
-        //string json = JsonUtility.ToJson(q1);
-        //Debug.Log(json);
-        //File.WriteAllText(Application.dataPath + "/QnA.json", json);
-
-
-        //path = Application.dataPath + "/QnA.json";
-        //string json = File.ReadAllText(path);
-        //QnA q1 = JsonUtility.FromJson<QnA>(json);
-        //Question.text = q1.Quesion;
-
 
         path = Application.dataPath + "/QnA.json";
         string json = File.ReadAllText(path);
         Course c1 = JsonUtility.FromJson<Course>(json);
         int n = int.Parse(c1.No_Of_Ques);
 
-        //if (int.TryParse(c, out n))
-        //{
-        //    print("amil4");
-        //    energyAmt.text = amt;
-        //}
-
-        //int qCounter = 0;
-
 
         StartQuestionRoutine(c1, n);
 
-        //SetRandom(c1.Ques_Data[0]);
-
-        //Debug.Log(c1.CourseName);
-        //Debug.Log(c1.No_Of_Ques);
-        //Debug.Log(c1.Ques_Data[1].options[0]);
-        //var list = (QnA)c1.Ques_Data[0];
-        //Debug.Log(list);
+        //ChangeButton();
 
     }
 
-    //private void Update()
-    //{
-    //    Course c1 = MakeJson();
-    //    StartCoroutine(QuestionRoutin(c1));
-    //}
+    public Course MakeJson()
+    {
+        path = Application.dataPath + "/QnA.json";
+        string json = File.ReadAllText(path);
+        Course c1 = JsonUtility.FromJson<Course>(json);
+        return c1;
+    }
 
-    //public Course MakeJson()
-    //{
-    //    path = Application.dataPath + "/QnA.json";
-    //    string json = File.ReadAllText(path);
-    //    Course c1 = JsonUtility.FromJson<Course>(json);
-    //    return c1;
-    //}
+    public void StartQuestionRoutine(Course c, int n)
+    {
+        StartCoroutine(QuestionRoutin(c, n));
+    }
 
-    //public void UpdateQuestion()
-    //{
-    //    Course c = MakeJson();
-    //    print("1" + qCounter);
-    //    Question.text = c.Ques_Data[qCounter].Quesion;
-    //    qCounter++;
-    //    print(qCounter);
-    //}
+    string amil;
+    //for changing questions
+    IEnumerator QuestionRoutin(Course c, int n)
+    {
+        while(qCounter < n)
+        {
+            yield return new WaitForSeconds(3.0f);
+            Question.text = c.Ques_Data[qCounter].Quesion;
+
+            for (int i = 0; i < b.Length -1 ; i++)
+            {
+                b[i].GetComponentInChildren<Text>().text = c.Ques_Data[qCounter].options[i];
+            }
+
+            b[3].GetComponentInChildren<Text>().text = c.Ques_Data[qCounter].Answer;
+            amil = c.Ques_Data[qCounter].Answer;
+
+            qCounter++;
+        }
+    }
+
+    public void ChangeButton()
+    {
+        for(int i =0; i < b.Length; i++)
+        {
+            b[i].name = "option" + i;
+        }
+    }
+
+
+    public void CheckAnswer()
+    {
+        if (EventSystem.current.currentSelectedGameObject.name == "Button (3)")
+        {
+            print("Chal gaya bc");
+
+            Animator anim = Correct_Panel.GetComponentInChildren<Animator>();
+            if (anim != null)
+            {
+                Correct_Panel.SetActive(true);
+                bool isRightAnsPressed = anim.GetBool("isRightAnsPressed");
+                anim.SetBool("isRightAnsPressed", !isRightAnsPressed);
+                Invoke("disableCorrectPanel", 1.0f);
+            }
+        }
+        else
+        {
+            Animator anim = Wrong_Panel.GetComponentInChildren<Animator>();
+            if (anim != null)
+            {
+                Wrong_Panel.SetActive(true);
+                bool isPressed = anim.GetBool("isPressed");
+                anim.SetBool("isPressed", !isPressed);
+                Invoke("disableWrongPanel", 1.0f);
+            }
+        }
+    }
+
+    public void disableWrongPanel()
+    {
+        Wrong_Panel.SetActive(false);
+    }
+
+    public void disableCorrectPanel()
+    {
+        Correct_Panel.SetActive(false);
+    }
+
+    public void disableWelcome()
+    {
+        Welcome_Panel.SetActive(false);
+    }
 
     public void SetRandom(Course c)
     {
@@ -284,84 +331,13 @@ public class QnA_json : MonoBehaviour
         return false;
     }
 
-    public void StartQuestionRoutine(Course c, int n)
+    private void NewNumber()
     {
-        StartCoroutine(QuestionRoutin(c, n));
-        //StartCoroutine(ButtonUpdate(c, qCounter, n));
+        MyNumber = a.Next(0, 4);
+        if (!randomList.Contains(MyNumber))
+            randomList.Add(MyNumber);
+        print("randome list: " + MyNumber);
     }
-
-    //for changing questions
-    IEnumerator QuestionRoutin(Course c, int n)
-    {
-        //print(qCounter);
-        //print("amil");
-        //qCounter++;
-        //print(qCounter);
-        while(qCounter < n)
-        {
-            print("n: " + n);
-            yield return new WaitForSeconds(3.0f);
-            Question.text = c.Ques_Data[qCounter].Quesion;
-
-            //for(int i =0; i< b.Length; i++)
-            //{
-            //    b[i].GetComponentInChildren<Text>().text = c.Ques_Data[i].options[i];
-            //}
-
-            //SetRandom(c);
-            print("Before" + qCounter);
-            qCounter++;
-            //b[qCounter].GetComponentInChildren<Text>().text = "";
-            //ButtonUpdate(c, qCounter);
-            print("After" + qCounter);
-        }
-    }
-
-    IEnumerator ButtonUpdate(Course c ,int qCounter, int n)
-    {
-        while(qCounter < n)
-        {
-            b[qCounter].GetComponentInChildren<Text>().text = c.Ques_Data[qCounter].Answer;
-            for (int i = 1; i < 4; i++)
-            {
-                b[i].GetComponentInChildren<Text>().text = c.Ques_Data[qCounter].options[i - 1];
-            }
-
-            yield return new WaitForSeconds(3.0f);
-        }
-    }
-
-    public void CheckAnswer(string q1)
-    {
-        print(q1);
-        for (int i = 0; i < b.Length; i++)
-        {
-            if (q1 == b[i].GetComponentInChildren<Text>().text)
-            {
-                Debug.Log("Correct Answer");
-            }
-            else
-            {
-                Debug.Log("Wrong Answer");
-                Animator anim = Wrong_Panel.GetComponentInChildren<Animator>();
-                if(anim != null)
-                {
-                    Wrong_Panel.SetActive(true);
-                    int c = 0;
-                    bool isPressed = anim.GetBool("isPressed");
-                    anim.SetBool("isPressed", !isPressed);
-                }
-                else
-                {
-                    Wrong_Panel.SetActive(false);
-                }
-                break;
-            }
-        }
-        print("bhir agaye loop se");
-    }
-
-
 
     [Serializable]
     public class QnA
